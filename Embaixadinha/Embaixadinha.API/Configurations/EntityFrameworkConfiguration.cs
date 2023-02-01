78a47b7147098bc67c1db5a2ccbfc6703ebee261
Marccus Zavadzki <marcuszavadzki72@gmail.com>
2023-01-31 22:23:10 -0300

    .

--- Embaixadinha/Embaixadinha.API/Configurations/EntityFrameworkConfiguration.cs
+++ Embaixadinha/Embaixadinha.API/Configurations/EntityFrameworkConfiguration.cs
@@ -7,7 +7,7 @@ namespace Embaixadinha.API.Configurations
     {
         public static void AddEntityFrameworkConfiguration(this IServiceCollection services, IConfiguration configuration)
         {
-            var connectionString = configuration["MYSQL_CONNECTION"];
+            var connectionString = configuration.GetConnectionString("MySqlConnection");
 
             services.AddDbContext<ApplicationContext>(options => {
                 options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)).UseSnakeCaseNamingConvention();
