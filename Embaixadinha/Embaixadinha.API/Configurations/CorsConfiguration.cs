e1acc84f9e644c9c50a59287170840f748c3fdf1
Marccus Zavadzki <marcuszavadzki72@gmail.com>
2023-01-31 22:50:19 -0300

    test cors.

--- Embaixadinha/Embaixadinha.API/Configurations/CorsConfiguration.cs
+++ Embaixadinha/Embaixadinha.API/Configurations/CorsConfiguration.cs
@@ -4,26 +4,17 @@
     {
 
         private static readonly string[] _urls = {
-            "https://jogos.marccusz.com",
-            "https://marccusz.com",
-            "https://www.marccusz.com",
-            "https://www.jogos.marccusz.com",
-            "https://embaixadinha.marccusz.com",
-            "https://www.embaixadinha.marccusz.com"
+            //"https://jogos.marccusz.com",
+            //"https://marccusz.com",
+            //"https://www.marccusz.com",
+            //"https://www.jogos.marccusz.com",
+            //"https://embaixadinha.marccusz.com",
+            //"https://www.embaixadinha.marccusz.com"
         };
 
         public static void AddCorsConfiguration(this IServiceCollection services)
         {
-            services.AddCors(options => {
-                options.AddPolicy("AllowOnlyMyDomains", builder => {
-                    builder
-                    .AllowAnyMethod()
-                    .AllowAnyHeader()
-                    .WithOrigins(_urls)
-                    .AllowCredentials();
-                });
-            });
-
+#if DEBUG
             services.AddCors(options => {
                 options.AddPolicy("AllowAll", builder => {
                     builder
@@ -33,6 +24,17 @@
                     .AllowCredentials();
                 });
             });
+#else
+            services.AddCors(options => {
+                options.AddPolicy("AllowOnlyMyDomains", builder => {
+                    builder
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .WithOrigins(_urls)
+                    .AllowCredentials();
+                });
+            });
+#endif
         }
 
         public static void UseCorsConfiguration(this IApplicationBuilder app)
